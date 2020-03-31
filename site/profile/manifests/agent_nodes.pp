15ee3f471110cb20f384592f7997a54238ca4e97
Netdemia <dhabbhoufaamou@gmail.com>
2020-03-30 21:05:06 -0500

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,14 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
+  
+  host { 'web.puppet.vm':
+    ensure => present,
+    ip     => '10.0.2.15',
+  }
+  
+  host { 'db.puppet.vm':
+    ensure => present,
+    ip     => '172.18.0.2',
+  }
 }
