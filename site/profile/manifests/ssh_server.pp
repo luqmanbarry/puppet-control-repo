921e59430dac5dff9659fc3730d5ea0e050eb779
Netdemia <dhabbhoufaamou@gmail.com>
2020-03-30 21:07:13 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -3,7 +3,7 @@ class profile::ssh_server {
     ensure => present,
   }
   service {'ssh':
-    ensure => runnig,
+    ensure => 'running',
     enable => true,
   }
   ssh_authorized_key { 'root@master.puppet.vm':
